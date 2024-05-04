cc6800edeb28fe5732289e568ac9da7789a9e8c4
Bhunmas <lovebhunma@outlook.com>
2024-05-04 17:27:18 +0700

    complete Cal

--- app.js
+++ app.js
@@ -1,133 +1,69 @@
-// let runningTotal=0;
-// let buffer="0";
-// let previousOperation;
-
-
-// const screen=document.querySelector(".screen");
-
-
-// function buttonClick(value){
-//     if(isNaN(value)){
-//         handleSymbol(value);
-//     }else{
-//         handleNumber(value);
-//     }
-//     screen.innerText=buffer;
-// }
-
-// function handleSymbol(Symbol){
-//     switch(Symbol){
-//         case "C":
-//             buffer="0";
-//             runningTotal=0;
-//             break;
-//         case "=":
-//             if(previousOperation==null){
-//                 return;
-//             } 
-//             flushOperation(parseInt(buffer));
-//             previousOperation=null;
-//             buffer=runningTotal;
-//             runningTotal=0;
-//             break;
-//         case "←":
-//             if(buffer.length === 1){
-//                 buffer="0";
-//             }else{
-//                 buffer=buffer.substring(0,buffer.length-1);
-//             }
-//             break;
-//         case "+":
-//         case "-":
-//         case "×":
-//         case "÷":
-//             handleMath(Symbol);
-//             break;
-//     }
-// }
-
-// function handleMath(Symbol){
-//     if(buffer==="0"){
-//         return;
-//     }
-
-//     const Intbuffer=parseInt(buffer);
-
-//     if(runningTotal===0){
-//         runningTotal=Intbuffer;
-//     }else{
-//         flushOperation(Intbuffer);
-//     }
-//     previousOperation=Symbol;
-//     buffer="0";
-
-// }
-// function flushOperation(Intbuffer){
-//     if(previousOperation==="+"){
-//         runningTotal+=Intbuffer;
-//     }else if(previousOperation==="-"){
-//         runningTotal-=Intbuffer;
-//     }else if(previousOperation==="×"){
-//         runningTotal*=Intbuffer;
-//     }else if(previousOperation==="÷"){
-//         runningTotal/=Intbuffer;
-//     }
-// }
-
-// function handleNumber(numberString){
-//     if(buffer==="0"){
-//         buffer=numberString;
-//     }else{
-//         buffer+=numberString;
-//     }
-// }
-
-// function init(){
-//     document.querySelector(".calc-buttons").addEventListener("click",function(event){
-//         buttonClick(event.target.innerText);
-//     });
-// }
-
-// init();
-
 runningTotal=0;
 buffer="0";
 let previousNumber =null;
+let Operator_sy=null;
 
 
 const Screen =document.querySelector(".screen");
 
-console.log(Screen.innerText);
-
-
-
 function HandleNumber(value){
-
-    buffer=value;
-    // previousNumber=previousNumber??value
-    console.log(buffer)
+    
+    if(buffer!== "0"){
+        buffer+=value;
+    }else{
+        buffer=value;
+    }
+    Screen.innerText=buffer;
 }
 
 function handleSymbol(Symbol){
-   
-   
-
+    
     switch(Symbol){
+        case "C":
+            CalClear()
+            break;
+        case "←":
+            buffer=buffer.length===1?"0":buffer.substring(0,buffer.length-1);
+            break;
         case "+":
-        case "-":
-        case "x":
+        case "−":
+        case "×":
         case "÷":
+            Operator_sy=previousNumber??Symbol;
             previousNumber=previousNumber??parseInt(buffer);
+            buffer="0";
             break;
         case "=":
-            console.log(previousNumber+parseInt(buffer));
+            CalOperator(Operator_sy)
             break;
+        
     }
+    Screen.innerText=buffer;
 
 }
-
-
-
+function CalClear(){
+    Operator_sy=null;
+    previousNumber =null;
+    runningTotal=0;
+    buffer="0";
+}
+function CalOperator(Operator){
+    if(Operator==="+"){
+        runningTotal=previousNumber+parseInt(buffer);
+        
+    }else if(Operator==="−"){
+        runningTotal=previousNumber-parseInt(buffer);
+      
+    }else if(Operator==="×"){
+        runningTotal=previousNumber*parseInt(buffer);
+       
+    }else if(Operator==="÷"){
+        runningTotal=previousNumber/parseInt(buffer)
+    }
+    
+    buffer=runningTotal
+   
+}
 function main(){
     const Btn=document.querySelectorAll('.calc-button');
     Btn.forEach( (value)=>{
